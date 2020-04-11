2ea067cab590aa89dc9e5b1c7bf71e249577f5e5
Adrian-Haba <56442732+Adrian-Haba@users.noreply.github.com>
2020-04-11 17:41:45 +0200

    Funcion 2 #2

---
+++ Funciones.java
@@ -0,0 +1,33 @@
+package pruebas;
+
+public class Funciones {
+
+	// Funcion 2
+	/*
+	 * Función que dada dos cadenas, diga cuál tiene más consonante. Debe hacerse
+	 * una prueba con tu nombre y un apellido. Y otra prueba con tus dos apellidos.
+	 */
+	public static Boolean funcion2(String cadena, String cadena2) {
+		int consonantes = 0;
+		for (int x = 0; x < cadena.length(); x++) {
+			char letra = cadena.charAt(x);
+			if (consonantescadenas(letra)) {
+				consonantes++;
+			}
+		}
+		int consonantes2 = 0;
+		for (int x = 0; x < cadena2.length(); x++) {
+			char letra = cadena2.charAt(x);
+			if (consonantescadenas(letra)) {
+				consonantes2++;
+			}
+		}
+		if (consonantes > consonantes2) {
+			return true;
+		} else if (consonantes < consonantes2) {
+			return false;
+		} else {
+			return null;
+		}
+	}
+}
